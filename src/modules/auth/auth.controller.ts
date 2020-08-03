6b37b49f26c8d4bd998722d2792146ada21526e8
mohammadqaderi <mqaderi44@gmail.com>
2020-08-03 19:03:19 +0300

    new update 3/8/2020

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -66,9 +66,9 @@ export class AuthController {
     const { id } = req.user.user;
     if (jwt) {
       res
-        .redirect(`http://localhost:4200/auth/google-success/userId:${id}/accessToken:${jwt}`);
+        .redirect(`https://music-land.herokuapp.com/auth/google-success/userId:${id}/accessToken:${jwt}`);
     } else {
-      res.redirect('http://localhost:4200/auth/google-failure');
+      res.redirect('https://music-land.herokuapp.com/auth/google-failure');
     }
   }
 
@@ -86,9 +86,9 @@ export class AuthController {
     const { id } = req.user.user;
     if (jwt) {
       res
-        .redirect(`http://localhost:4200/auth/facebook-success/userId:${id}/accessToken:${jwt}`);
+        .redirect(`https://music-land.herokuapp.com/auth/facebook-success/userId:${id}/accessToken:${jwt}`);
     } else {
-      res.redirect('http://localhost:4200/auth/facebook-failure');
+      res.redirect('https://music-land.herokuapp.com/auth/facebook-failure');
     }
   }
 
